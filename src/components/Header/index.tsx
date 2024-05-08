2cf4c6a1bf7c4ab3044f41a47b02b98930a02762
Gabriel Caputo <caputomateus@gmail.com>
2024-05-08 00:10:32 -0300

    Adiciona fetch pra pegar o nome da cidade via IP

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,28 @@
+import { useEffect, useState } from "react"
+
+interface LocationType {
+  city: string,
+  region: string,
+}
+
 export function Header() {
+  const [location, setLocation] = useState<LocationType>({
+    city: '',
+    region: ''
+  })
+  const locationText = location.city.length > 0 && `${location.city}, ${location.region}`
+
+  useEffect(() => {
+    fetch("http://ip-api.com/json/", {
+      method: 'GET',
+    }).then(response => response.json())
+      .then(result => {
+        setLocation(result)
+      })
+      .catch(e => e);
+  }, [])
+
   return (
-    <>Header</>
+    <>{locationText}</>
   )
 }
